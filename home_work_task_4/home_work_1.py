b7018216af2bcd0020d994eb9fad84e093b25243
Valentin <iv.valentin.ua@gmail.com>
2025-06-15 15:11:50 +0100

    Home_work_1

--- home_work_task_4/home_work_1.py
+++ home_work_task_4/home_work_1.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+from datetime import datetime
+
+def get_days_from_today(date:str):
+    try:
+        # Перетворення рядка у дату
+        users_date = datetime.strptime(date, "%Y-%m-%d").date()
+        # Отримання поточної дати
+        current_date = datetime.now().date()
+        # Різниця у днях
+        days_since = (current_date - users_date).days
+        return days_since
+    except ValueError as e:
+        print(f"Invalid date format: {e}")
+
+print(get_days_from_today("2024-06-10"))
